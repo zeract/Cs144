0f7d245aa596e3a10ec5a91724a0f794447f13c7
zeract <583035146@qq.com>
2023-02-23 04:00:32 -0800

    lab1

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -6,9 +6,9 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-struct store_data{
-  size_t index;
-  std::string _queue;
+struct store_data {
+    size_t index;
+    std::string _queue;
 };
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -16,12 +16,12 @@ struct store_data{
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
-    size_t _current_index=0; //!< 现在的最后一位索引号
-    size_t _eof_index=0;
-    bool _eof=false;
-    std::vector<struct store_data> _store={};  //!<  存储未排列的字符流
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    size_t _current_index = 0;  //!< 现在的最后一位索引号
+    size_t _eof_index = 0;
+    bool _eof = false;
+    std::vector<struct store_data> _store = {};  //!<  存储未排列的字符流
+    ByteStream _output;                          //!< The reassembled in-order byte stream
+    size_t _capacity;                            //!< The maximum number of bytes
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
